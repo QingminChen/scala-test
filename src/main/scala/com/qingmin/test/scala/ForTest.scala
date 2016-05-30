8478e1cbc61cdbd98bc0191435bbf0452b0a6a0c
qingmin.chen <qingmin.chen@accenture.com>
2016-05-30 22:50:59 +0800

    update the new code

---
+++ src/main/scala/com/qingmin/test/scala/ForTest.scala
@@ -0,0 +1,12 @@
+package com.qingmin.test.scala
+
+/**
+  * Created by qingmin.chen on 5/30/2016.
+  */
+object ForTest extends App{
+  println(1.to(10))
+  for(i<-1 to 10){   //这里就会int类型的数值的to方法
+    println(i)
+  }
+
+}
