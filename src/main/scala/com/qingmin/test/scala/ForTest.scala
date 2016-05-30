f952dfb2e0aa50b27cf84a12fef6d77a1aacf298
qingmin.chen <qingmin.chen@accenture.com>
2016-05-30 23:07:37 +0800

    update the new code

--- src/main/scala/com/qingmin/test/scala/ForTest.scala
+++ src/main/scala/com/qingmin/test/scala/ForTest.scala
@@ -5,8 +5,19 @@ package com.qingmin.test.scala
   */
 object ForTest extends App{
   println(1.to(10))
-  for(i<-1 to 10){   //这里就会int类型的数值的to方法
+  for(i<-1 to 10){   //这里就会int类型的数值的to方法  这个是两边都是闭区间
     println(i)
   }
 
+  for(i<-1 until 10){//这是半开区间，左闭右开
+    println(i)
+  }
+
+  for(i<-1 to 10 reverse){//这个写法等同于 i<- (1 to 10).reverse
+    println(i)
+  }
+
+  for(i<-"superman"){//遍历字符串
+    println(i)
+  }
 }
